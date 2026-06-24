765721ff390bf817e5e0449228d5f920ff581ea4
Márton Leitold <83549873+Tomagazda@users.noreply.github.com>
2026-06-24 14:51:33 +0200

    Json upload- menu

---
+++ assets/scripts/menu.js
@@ -0,0 +1,69 @@
+let menu =[
+
+        {
+        name:"Tomatensuppe",
+        price:2,
+        description:"Eine cremige Tomatensuppe ist eine warme, erfrischende Suppe aus gekochten und pürierten Tomaten, oft mit Zwiebeln, Knoblauch und Kräutern verfeinert."
+    },
+    
+     {
+        name: "Knoblauchcremesuppe",
+        price: 3,
+        description: "Knoblauchcremesuppe ist eine aromatische, samtige Suppe, die aus sanft geschmorten Knoblauchzehen besteht."
+    },
+    
+     {
+        name: "Gulaschsuppe",
+        price: 4,
+        description: "Gulaschsuppe ist eine herzhafte, kräftige Fleischsuppe mit zarten Rindfleischstücken, Gemüse und einer intensiven Gewürznote aus Paprika und anderen aromatischen Kräutern."
+    },
+    
+     {
+        name: "Gebackene Trappistenkäse",
+        price: 5,
+        description: "Gebackener Trappistenkäse ist eine knusprige, goldbraune Vorspeise, bei der ein Block reifer, halbfester Käse aus dem Trappistenkloster im Ofen oder Fritteuse geschmolzen wird."
+    },
+    
+     {
+        name: "Brassauer Schweinebraten",
+        price: 6,
+        description: "Ein traditionelles Siebenbürger Fleischgericht aus Rumänien, bei dem ein saftiges Schweinefleischstück langsam mit Zwiebeln, Paprika und oft Sauerrahm gegart wird, um eine intensive, aromatische Geschmacksnote zu erzielen."
+    },
+    
+     {
+        name: "Rindergulasch mit Nockerln",
+        price: 7,
+        description: "Rindergulasch mit Nockerln ist ein herzhaftes Hauptgericht, das zarte Rindfleischstücke in einer kräftigen, gewürzten Paprika-Zwiebelsauce mit weichen Semmelknödeln kombiniert."
+    },
+    
+     {
+        name: "Gebratene Forelle mit Röstkartoffeln",
+        price: 6,
+        description: "Gebratene Forelle mit Röstkartoffeln ist ein schmackhaftes Gericht aus knusprig gebratenem Fischfilet, das auf saftigen, goldbraun gerösteten Kartoffeln serviert wird."
+    },
+    
+     {
+        name: "Krautsalat",
+        price: 3,
+        description: "Krautsalat ist eine erfrischende, säuerliche Beilage aus fein geschnittenem oder geraspeltem Weißkohl, der mit Essig, Öl, Zwiebeln und Gewürzen mariniert wird."
+    },
+    
+     {
+        name: "Hausgemachter gemischter Salat",
+        price: 3,
+        description: "Hausgemachter gemischter Salat ist eine frische, lebendige Beilage aus einer Auswahl sorgfältig gewählter Blattsalate und saisonaler Gemüse, die oft mit einem leichtem Essig-Öl-Dressing serviert wird."
+    },
+    
+     {
+        name: "Salzgurken",
+        price: 3,
+        description: "Salzgurken sind knusprige, eingelegte Gurken, die in einer salzigen Lake durch Fermentation oder Essigsud aromatisch gewürzt und haltbar gemacht wurden."
+    },
+
+      {
+        name: "Tomatensalat",
+        price: 3,
+        description: "Tomatensalat ist eine frische, erfrischende Beilage aus reifen, saftigen Tomatenstücken, die mit Salz, Pfeffer, Öl und oft Zwiebeln oder Kräutern verfeinert wird."
+    },
+    
+]
